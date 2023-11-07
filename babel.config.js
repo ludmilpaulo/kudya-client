b501f5129dced3f76a3aa8d701cd7b032a86d087
ludmilpaulo <ludmilpaulo@gmail.com>
2023-11-07 21:04:00 +0200

    babel

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,26 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: ["tailwindcss-react-native/babel"],
+    presets: [
+      [
+        'module:metro-react-native-babel-preset',
+        {
+          unstable_disableES6Transforms: true,
+        },
+      ],
+     // 'module:metro-react-native-babel-preset',
+      // Add the following preset for web support
+      'babel-preset-expo',
+    ],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          extensions: ['.tsx', '.ts', '.js', '.json'],
+        },
+      ],
+      'react-native-reanimated/plugin',
+      'tailwindcss-react-native/babel',
+    ],
   };
 };
